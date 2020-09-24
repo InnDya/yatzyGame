3b910568bdc2603cb506abb377e96ae580706bcd
Sebastian Orbom <zebban90a@gmail.com>
2020-09-24 14:46:58 +0200

    changes again in game.js

--- js/game.js
+++ js/game.js
@@ -21,19 +21,24 @@ class Game {
             let partsum_field = document.querySelector(".sum1 .player" + player);
             partsum_field.innerHTML = sum;
 
-            let bonus =0;
-            if (sum >= 63) {
-        
-            var bonus_field = document.querySelector(".bonus .player" + player);
-            bonus.value =50;
-                
             
-            }
-            bonus_field.innerHTML = bonus;
+            let bonus =0;
 
+             if (sum >= 63) {
+                 bonus = 50;  
+                
+             }               
+             var bonus_field = document.querySelector(".bonus .player" + player);
+             bonus_field.innerHTML = bonus;
+            
             let totalSum_field = document.querySelector(".totalsum .player" + player);
-            totalSum_field.innerHTML = sum + bonus_field;
-            console.log(totalSum_field);
+            totalSum_field.innerHTML = sum + bonus;
+
+            
+
+            //let totalSum_field = document.querySelector(".totalsum .player" + player);
+            //totalSum_field.innerHTML = sum + bonus_field;
+            //console.log(totalSum_field);
         }
     }
     
